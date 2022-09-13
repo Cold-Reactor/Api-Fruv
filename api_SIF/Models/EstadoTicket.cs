152a3abbff568d61ea7ab3ecb9ac6f31b4aa7075
Elnatan Jaziel Manjarrez Albarran <jaziel.manjarrez@cenace.gob.mx>
2022-09-13 14:22:08 -0700

    Agregar archivos de proyecto.

---
+++ api_SIF/Models/EstadoTicket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_SIF.Models
+{
+    public class EstadoTicket
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public string tipo { get; set; }
+    }
+}
---
+++ api_SIF/Startup.cs
@@ -0,0 +1,65 @@
+using api_SIF.dbContexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_SIF
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string mySqlConnectionStr = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MyDBContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(mySqlConnectionStr, null)
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
