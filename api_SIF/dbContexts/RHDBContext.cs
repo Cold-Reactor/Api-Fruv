e33848aa392a924cec845726df57252edc0bb506
jaziel desktop <jazie@DESKTOP-UGTGL7B>
2023-02-01 21:50:42 -0800

    +se quito configuracion de bd

--- api_SIF/dbContexts/RHDBContext.cs
+++ api_SIF/dbContexts/RHDBContext.cs
@@ -19,14 +19,6 @@ namespace api_SIF.dbContexts
 
         public virtual DbSet<empleado> empleados { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=sistema.fruvemex.com;port=3306;database=empleadosN;user=jaziel;password=902015jz", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.19-mysql"));
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
