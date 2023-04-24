a1efca479b3b21d901566a86ed4f6d49e3691d5d
jaziel pc <jazie@DESKTOP-PKD70HN>
2023-04-23 20:01:59 -0700

    +se agrego foliomp

---
+++ api_SIF/Models/MateriaPrima/foliomp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_SIF.Models.MateriaPrima
+{
+    [Table("foliomp")]
+    [Index("identradadetalle", Name = "identradadetalle")]
+    [MySqlCharSet("utf8")]
+    [MySqlCollation("utf8_spanish_ci")]
+    public partial class foliomp
+    {
+        [Key]
+        [Column(TypeName = "int(11)")]
+        public int id { get; set; }
+        [StringLength(11)]
+        [MySqlCollation("utf8_general_ci")]
+        public string folio { get; set; }
+        public bool? disponible { get; set; }
+        [Column(TypeName = "int(11)")]
+        public int? identradadetalle { get; set; }
+    }
+}
