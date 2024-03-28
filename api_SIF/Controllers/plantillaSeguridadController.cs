91229e760405c76e5ea0656fa0feef61c3b39c2b
PC SIF <Admin@DESKTOP-H0LQCS6>
2024-03-28 16:01:26 -0700

    Actualizacion de Plantillas

--- api_SIF/Controllers/plantillaSeguridadController.cs
+++ api_SIF/Controllers/plantillaSeguridadController.cs
@@ -64,7 +64,7 @@ namespace api_SIF.Controllers
 
             var empleados = await (from p in _context.empleados
                                      join a in _context.areas on p.id_area equals a.id_area
-                                     where p.status == 1 && p.id_sucursal == id_sucursal && a.id_departamento == 1
+                                     where p.status == 1 && p.id_sucursal == id_sucursal && a.id_departamento == 11
                                      select new RequestPlantillaGuardia
                                      { id_empleado = p.id_empleado,
                                        nombre = p.apellidoPaterno + " " + p.apellidoMaterno + " " + p.nombre,
@@ -94,7 +94,7 @@ namespace api_SIF.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<plantillaSeguridad>> PostPlantillaS(List<plantillaSeguridad> plantillas)
+        public async Task<ActionResult<string>> PostPlantillaS(List<plantillaSeguridad> plantillas)
         {
             //_context.plantillas.Add(plantilla);
             //await _context.SaveChangesAsync();
@@ -107,7 +107,7 @@ namespace api_SIF.Controllers
             }
                 await _context.SaveChangesAsync();
 
-            return Ok("Plantilla Creada");
+            return "Plantilla Creada";
         }
 
         [HttpPut]
