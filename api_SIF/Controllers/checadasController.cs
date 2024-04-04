75d13a84ef5e09d9a18491af78e5a18b2aa7ea24
PC SIF <Admin@DESKTOP-H0LQCS6>
2024-04-04 12:26:48 -0700

    se mofico el cumpleaños en

--- api_SIF/Controllers/checadasController.cs
+++ api_SIF/Controllers/checadasController.cs
@@ -235,20 +235,30 @@ namespace api_SIF.Controllers
                             checadaEmpleado.ausentismo = "Incapacidad";
                         }
                     }
-                    if (checksEmpleado.Count == 0)
-                    {
-                        var fechaNacimiento = _context.empleados.FirstOrDefault(e => e.id_empleado == empleado.id_empleado).fechaNacimiento.Value;
-                        //convierte la fecha de nacimiento a DateTime
+                    var fechaNacimiento = empleado.fechaIngreso.Value;
+                    //convierte la fecha de nacimiento a DateTime
 
-                        if (fechaNacimiento!=null) {
-                            if (fechaNacimiento.Month == fecha.Month && fechaNacimiento.Day == fecha.Day)
-                            {
-                                checadaEmpleado.ausentismo = "Cumpleaños";
-                            }
+                    if (fechaNacimiento != null)
+                    {
+                        if (fechaNacimiento.Month == fecha.Month && fechaNacimiento.Day == fecha.Day)
+                        {
+                            checadaEmpleado.ausentismo = "Cumpleaños";
                         }
                     }
+                    //if (checksEmpleado.Count == 0)
+                    //{
+                    //    var fechaNacimiento = _context.empleados.FirstOrDefault(e => e.id_empleado == empleado.id_empleado).fechaNacimiento.Value;
+                    //    //convierte la fecha de nacimiento a DateTime
 
-                    
+                    //    if (fechaNacimiento!=null) {
+                    //        if (fechaNacimiento.Month == fecha.Month && fechaNacimiento.Day == fecha.Day)
+                    //        {
+                    //            checadaEmpleado.ausentismo = "Cumpleaños";
+                    //        }
+                    //    }
+                    //}
+
+
 
 
 
